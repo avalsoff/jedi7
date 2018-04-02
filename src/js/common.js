5ee0cfde76b031660658efe453d1088a6508c242
avalsoff <avalsoff@gmail.com>
2018-04-03 00:27:50 +1000

    Add mmenu and style it

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
+  $('#my-menu').mmenu({
+    extensions: [  'widescreen', 'theme-black', 'fx-menu-slide', 'pagedim-black', 'position-right' ],
+    navbar: {
+      title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+    },
+    hooks: {
+      'open:finish': function( $panel ) {
+        $('.hamburger').addClass('is-active');
+      },
+      'close:finish': function( $panel ) {
+        $('.hamburger').removeClass('is-active');
+      }
+    }
+  });
 
-	// Custom JS
+  // var api = $('#my-menu').data('mmenu');
+  // api.bind('open:start', function () {
+  //   $('.hamburger').addClass('is-active');
+  // }).bind('close:finish', function () {
+  //   $('.hamburger').removeClass('is-active');
+  // });
 
 });

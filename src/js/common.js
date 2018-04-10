649f9ca1bee98a558c96ffce7456f2bd5aa0e6ac
avalsoff <avalsoff@gmail.com>
2018-04-10 22:46:32 +1000

    Add scroll to top button and preloader

--- src/js/common.js
+++ src/js/common.js
@@ -99,4 +99,19 @@ $(function() {
   }onResize();
   window.onresize = function() {onResize()}
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(this).height()) {
+      $('.top').addClass('active');
+    } else {
+      $('.top').removeClass('active');
+    }
+  });
+  $('.top').click(function() {
+    $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+  })
+
 });
+
+$(window).on('load', function() {
+  $('.preloader').delay(1000).fadeOut('slow');
+})
